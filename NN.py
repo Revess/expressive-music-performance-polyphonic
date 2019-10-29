27ecfde51a30120d172c5786eca48ee6a8cac730
kodai0422 <kodai0422@lee-lab.org>
2019-10-29 13:16:34 +0100

    latest

--- NN.py
+++ NN.py
@@ -10,24 +10,27 @@ col = ["sp"+str(n) for n in range(256)]
 col.append("Pitch_YIN")
 
 data = pd.read_csv("input1.csv", names = col)
-# target = pd.read_csv("input2.csv", names = ["Pitch_manual"])
 target = pd.read_csv("manual_mid_edit.csv", names = ["Pitch_manual","power"])
 target = target.drop(["power"], axis=1)
 target = target[:len(data)]
 target = np.ravel(target)
 
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=0)
 clf = MLPClassifier()
 
 # 試行するパラメータを羅列する
 params = {
-    "momentum": [(n+1)*0.1 for n in range(10)],
-    "alpha":[0],
-    # "hidden_layer_sizes":[],
+    "max_iter":[300],
+    "early_stopping":[True],
+    "momentum": [0.005],
+    "alpha":[5],
+    "hidden_layer_sizes":[(215,188,170,158),(215,188,170,158,150),(215,188,170,158,150,145)],
     # "baich_size":[],
 }
 grid_search = GridSearchCV(clf, param_grid=params, cv=5, n_jobs=-1,)
 grid_search.fit(data, target)
 
+with open("result_param.txt","a",encoding="UTF-8") as f:
+    f.write(grid_search.best_score_)
+    f.write(grid_search.best_params_)
 print(grid_search.best_score_)  # 最も良かったスコア
 print(grid_search.best_params_)  # 上記を記録したパラメータの組み合わせ
