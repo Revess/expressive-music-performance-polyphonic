fa0912aefee8d5bc3d335f28b99d557bc2c92d0b
Bas <bjmaat@gmail.com>
2019-11-11 10:36:16 +0100

    Testing and improving test scores

--- library/Generate_Files.py
+++ library/Generate_Files.py
@@ -14,15 +14,19 @@ def generate_files(GenerateMidi = False, GenerateSpec = False, GenerateLabels = 
     #Paths
     MIDI_CSV = os.path.join('.','Data','Csv')
     MIDI_SCORE = os.path.join('.','Data','Csv','ScoreMidi.csv')
-    MIDI_EDITED = os.path.join('.','Data','Csv','EditedScore.csv')
+    MIDI_EDITED = os.path.join('.','Data','Csv','EditedScore1CT.csv')
+    MIDI2_EDITED = os.path.join('.','Data','Csv','EditedScore2AT.csv')
     MIDI_PATH = os.path.join('.','Data','Midi')
     OUTPUT_PATH = os.path.join('.','Data','Output','pred_midi.mid')
     PRED_MIDI_PATH = os.path.join('.','Data','Output','research.csv')
     SPEC_PATH = os.path.join('.','Data','Csv','spectrum.csv')
     SPEC2_PATH = os.path.join('.','Data','Csv','spectrum2.csv')
-    AUDIO_PATH = os.path.join('.','Data','Audio','S01-AT.wav')
+    SPEC3_PATH = os.path.join('.','Data','Csv','spectrum3.csv')
+    AUDIO_PATH = os.path.join('.','Data','Audio','S01-CT.wav')
     AUDIO2_PATH = os.path.join('.','Data','Audio','S01-BT.wav')
+    AUDIO3_PATH = os.path.join('.','Data','Audio','S01-AT.wav')
     LABELS_PATH = os.path.join('.','Data','Csv','labels.csv')
+    LABELS2_PATH = os.path.join('.','Data','Csv','labels2.csv')
 
     #Write MIDI CSV
     if(midi):
@@ -39,11 +43,19 @@ def generate_files(GenerateMidi = False, GenerateSpec = False, GenerateLabels = 
         print("Transforming: " + str(AUDIO2_PATH))
         asc.audio_to_spectroCSV(AUDIO2_PATH,SPEC2_PATH,window,overlapping,remove_silence=removeSilence,Show_Graph=ShowGraph,Write_File=WriteFile)
         print("\n")
+        print("Transforming: " + str(AUDIO3_PATH))
+        asc.audio_to_spectroCSV(AUDIO3_PATH,SPEC3_PATH,window,overlapping,remove_silence=removeSilence,Show_Graph=ShowGraph,Write_File=WriteFile)
+        print("\n")
 
     #Writing LABELS CSV
     if(labels):
         print("~~~~Labels to CSV~~~~")
+        print("Transforming: " + str(LABELS_PATH))
         sotc.find_and_write_labels(SPEC_PATH,MIDI_EDITED,LABELS_PATH)
+        print("\n")
+        print("~~~~Labels to CSV~~~~")
+        print("Transforming: " + str(LABELS2_PATH))
+        sotc.find_and_write_labels(SPEC3_PATH,MIDI2_EDITED,LABELS2_PATH)
         print("\n")
 
     if(output and not labels and not spectral):
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ sys.path.insert(1,os.path.join('library'))
 import Generate_Files as gf
 import NN as nn
 
+# print("===========Generating_MIDI===========")
+# gf.generate_files(GenerateMidi=True)
 print("===========Generating_Inputs===========")
-gf.generate_files(GenerateLabels=True,GenerateSpec=True,WriteFile=True,window=2048*4,overlapping=10)
+gf.generate_files(GenerateLabels=True,GenerateSpec=True,WriteFile=True,window=2048*8,overlapping=4)
 print("===========Training===========")
-nn.NN(hiddenlayers=[110,125])
+nn.NN(hiddenlayers=[int(10837/4),int(10837/4),100])
 print("===========Generating_Output===========")
 gf.generate_files(GenerateOutput=True)
