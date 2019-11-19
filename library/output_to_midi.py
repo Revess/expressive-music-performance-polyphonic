5c0c6d9d26ff2d5dbc8d9763a894d60a5f8ac86c
Bas <bjmaat@gmail.com>
2019-11-19 12:53:24 +0100

    Trying out dtw, not working yet

--- library/output_to_midi.py
+++ library/output_to_midi.py
@@ -2,10 +2,58 @@ import numpy as np
 import pandas as pd
 from midiutil.MidiFile import MIDIFile
 import time as t
+from dtw import *
+import os
+
+MIDIDATACSV = os.path.join('Data','Csv','ScoreMidi.csv')
+
+def rescale_midi(midiDataCsv,midiLabels):
+    score = pd.read_csv(midiDataCsv)
+    labels = midiLabels
+    x_score = np.array([],dtype=int)
+    y_labels = np.array([],dtype=int)
+    start_silence = np.array([])
+    timeslice = labels["time in seconds"].to_numpy(dtype=float)
+    labels = labels.drop(["time in seconds"],1)
+    notes = labels.columns.values.astype(dtype=int)
+    scaling_factor = timeslice[int(timeslice.shape[0])-1]/score.loc[int(score.shape[0])-1,"Onset_s"]
+
+    #transform score data into an array of same length midinotes
+    #stretch the length of the data to match labels length
+    if(int(timeslice.shape[0]) != int(score.shape[0])):
+        x_score = np.append(x_score,score.loc[0,"Pitch_MIDI"])
+        for i in range(int(labels.shape[0])):
+            for j in range(int(score.shape[0])):
+                if((score.loc[j,"Onset_s"]*scaling_factor) <= timeslice[i] and ((score.loc[j,"Onset_s"] + score.loc[j,"Duration_s"])*scaling_factor) >= timeslice[i]):
+                    x_score = np.append(x_score,score.loc[j,"Pitch_MIDI"])
+    #transform labels into an array of midi notes
+    for i in range(int(labels.shape[0])):
+        stepcounter = 0
+        for j in range(int(labels.shape[1])):
+            value = labels.iloc[i,j]
+            if(value != 0):
+                y_labels = np.append(y_labels,notes[j])
+            else:
+                stepcounter += 1
+        if(stepcounter == 128):
+            y_labels = np.append(y_labels,128)
+    # x_score = x_score.reshape(-1,1)
+    # y_labels = y_labels.reshape(-1,1)
+    print(x_score,y_labels)
+    euclidean_norm = lambda x, y: np.abs(x_score - y_labels)
+    d, cost_matrix, acc_cost_matrix, path = dtw(x_score, y_labels, dist=euclidean_norm)
+    import matplotlib.pyplot as plt
+
+    plt.imshow(acc_cost_matrix.T, origin='lower', cmap='gray', interpolation='nearest')
+    plt.plot(path[0], path[1], 'w')
+    plt.show()
+    return 0
 
 def output_to_midi(OUTPUT_PATH,PRED_MIDI_PATH):
     print("reading csv file...")
     predmidi = pd.read_csv(PRED_MIDI_PATH)
+    rescale_midi(MIDIDATACSV,predmidi)
+
     timeslice = predmidi["time in seconds"].to_numpy(dtype=float)
     predmidi = predmidi.drop(["time in seconds"],1)
     mididata = [[i for i in range(128)]]
