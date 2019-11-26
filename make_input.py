7461213251b0541182c39c6252eadc9bafdb79ba
kodai0422 <kodai0422@lee-lab.org>
2019-11-26 11:33:48 +0100

    latest

--- make_input.py
+++ make_input.py
@@ -2,5 +2,5 @@ from func import midi_edit, audiospectrum_to_csv
 
 samplingrate = 4096
 timeflame = audiospectrum_to_csv.audio_to_spectroCSV("audio/Cello_Suite_1007_mono.wav","doc/spec.csv",samplingrate,0.1 ,False,False,True)
-midi_edit.midi_edit("doc/manual_mid.csv","doc/manual_mid_edit.csv",timeflame)
+midi_edit.midi_edit("manual_mid.csv","doc/manual_mid_edit.csv",timeflame)
 
