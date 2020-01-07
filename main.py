5afc79ea003d70a0e7b264e98cb289f624377152
Bas <bjmaat@gmail.com>
2020-01-07 14:55:58 +0100

    x

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ sys.path.insert(1,os.path.join('library'))
 import Generate_Files as gf
 import NN as nn
 
-# print("===========Generating_MIDI===========")
-# gf.generate_files(GenerateMidi=True)
-# print("===========Generating_Inputs===========")
-# gf.generate_files(GenerateLabels=True,GenerateSpec=True,WriteFile=True,window=11025,overlapping=0.25/16)
+print("===========Generating_MIDI===========")
+gf.generate_files(GenerateMidi=True)
+print("===========Generating_Inputs===========")
+gf.generate_files(GenerateLabels=True,GenerateSpec=True,WriteFile=True,window=11025,overlapping=0.25/16)
 print("===========Training===========")
 nn.NN(numNodes=128)
 print("===========Generating_Output===========")
